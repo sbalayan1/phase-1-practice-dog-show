fdafc11223c6b2c35513ee1b72fb586f5088a544
sbalayan1 <balayans2014@yahoo.com>
2022-11-15 14:05:29 -0800

    update' git push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,98 @@
-document.addEventListener('DOMContentLoaded', () => {
+fetchDogs().then(data => {data.forEach(dog => {renderDog(dog)})})
+document.getElementById('dog-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    for (let child of e.target.children) {
+        if (child.value === "") {
+            console.log('please provide all inputs')
+            return
+        }
+    }
+
+    const form = e.target
+    const dogInfo = {
+        name: form["name"].value, 
+        breed: form["breed"].value, 
+        sex: form["sex"].value
+    }
+
+    fetchDogs()
+    .then(data => {
+        const foundDog = data.find(dog => dog.name === dogInfo.name)
+        foundDog ? updateDogInfo(dogInfo, foundDog) : createDog(dogInfo)        
+    })
 
 })
+
+function fetchDogs() {
+    return fetch('http://localhost:3000/dogs')
+    .then(res => res.json())
+}
+
+function renderDog(dog) {
+    const table = document.getElementById('table-body')
+    const tableRow = document.createElement('tr')
+    const name = document.createElement('td')
+    const breed = document.createElement('td')
+    const sex = document.createElement('td')
+    const buttonContainer = document.createElement('td')
+    const button = document.createElement('button')
+
+    name.textContent = dog.name
+    breed.textContent = dog.breed
+    sex.textContent = dog.sex
+    button.textContent = "edit"
+
+    button.addEventListener('click', () => {
+        populateForm(dog)
+    })
+
+    buttonContainer.append(button)
+    tableRow.append(name, breed, sex, buttonContainer)
+    table.append(tableRow)
+}
+
+function populateForm(dog) {
+    const form = document.getElementById('dog-form')
+    let i = 0
+    while (i < form.length-1) {
+        const child = form[i]
+        child.value = dog[child.name]
+        i++
+    }
+
+}
+
+function createDog(dogInfo) {
+    fetch(`http://localhost:3000/dogs`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(dogInfo)
+    })
+    .then(res => res.json())
+    .then(data => {renderDog(data)})
+}
+
+function updateDogInfo(dogInfo, dog) {
+    console.time()
+    fetch(`http://localhost:3000/dogs/${dog.id}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(dogInfo)
+    })
+    .then(() => {
+        const row = document.getElementById('table-body').children[dog.id-1].children
+        const values = Object.values(dogInfo)
+        let i = 0
+        while (i<row.length-1) {
+            row[i].textContent = values[i]
+            i++
+        }
+        console.timeEnd()
+    })
+}
